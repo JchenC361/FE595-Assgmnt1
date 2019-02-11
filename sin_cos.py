1fe5ff9489664b171560ac2664297bdd01846a2b
JchenC361 <46977138+JchenC361@users.noreply.github.com>
2019-02-11 04:24:29 -0500

    Update sin_cos.py
    Add a tangent line and save image of sin, cos and tan

--- sin_cos.py
+++ sin_cos.py
@@ -4,6 +4,19 @@ import matplotlib.pyplot as plt
 time = np.arange(0,2*np.pi,0.1)
 sin = np.sin(time)
 cos = np.cos(time)
+
+# Limit the range of y axis between -1 to 1.
+plt.ylim((-1, 1))
+
 plt.plot(time, sin, time, cos)
 plt.axhline(y=0, color = 'black')
+# Plot tangent line.
+plt.plot(time, tan, color='grey')
+
+# Generate figure before plt.show in order to avoid outputting blank image.
+fig = plt.gcf()
+
+# Save the created image in your local directory.
+fig.savefig('D:/SinCosTan.png')
+
 plt.show()
